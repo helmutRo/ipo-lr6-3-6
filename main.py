f01762b43fe2505fff793c386d48913aa6b28db7
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-10-27 14:46:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
-from itertools import combinations
-number = [random.randint(-10, 10) for i in range(4)for i in range(20)] #создает список
-print("список: ", number) #выводится список
-unic = list(combinations(number, 2))
-print("уникальные пары: ", unic)
-a = list(input("введите целое число: "))
-scet = sum(1 for para in unic if sum(para) < a)
-print(f"положительные элементы  {a}: {scet} ") #вывод колва пол знач
-
+numbers = [0] * 20 #список 
+unic = [] #список уникальных пар
+kolvo = 0 #количество пар, чья сумма меньше заданного пользователем значения.
+for i in range(20): #заполнение списка и кортежей рандомными числами
+    numbers[i] = tuple(sorted([random.randint(-10, 10) for a in range(4)]))  
+for i in numbers: #проверка уникальных пар и добавление к списку
+    if numbers.count(i) == 1:
+        unic.append(i)
+print("Все уникальные пары: ", unic) #вывод уникальных 
+b = int(input("Введите целое число: ")) ##ввод Пользователем целого числа
+for i in numbers: #колво пар чья сумма меньше b
+    kolvo +=(1 if i[0] + i[1] + i[2] + i[3] < b else 0)
+print(f"Количество пар, чья сумма < {b} = {kolvo}") #вывод колва
